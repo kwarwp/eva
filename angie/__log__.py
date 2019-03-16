0ef83969900110fd5030adee201777d93713e4f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:24:25 -0300

    Saved angie/__log__.py at Timestamp: 2019-03-16 14:24:25

--- angie/__log__.py
+++ angie/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                                                                                                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Mar 16 2019 11:24:25.433 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  cenaAldeiadaAreia = (img = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi92_G17obhAhWYILkGHQl_AvAQjRx6BAgBEAU&url=https%3A%2F%2Faminoapps.com%2Fc%2Fnaruto-shippuden-online%2Fpage%2Fblog%2Fterceira-guerra-ninja%2Fq3Xl_37iRuBLWdb7dN5DrXekq7re6Gx1Wa&psig=AOvVaw1fQ3zN2UVVOCTb8XYKVE62&ust=1552831209833668"
+                                                                                                                                                                                                                                                                                                                                                   ^
+SyntaxError: invalid syntax
+'''},
