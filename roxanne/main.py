2b33f117128763e003247452d228d78131586d6b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:50:50 -0300

    Saved roxanne/main.py at Timestamp: 2019-03-16 13:50:49

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,3 +1,6 @@
 #michelle
 from _vittolino.main import Cena,Elemeto,Texto
 linkFaniquita ="https://i.pinimg.com/originals/53/48/7d/53487d3650ef3eb22f3c8f29ad1e09a9.png"
+def historia():
+
+Historia()
