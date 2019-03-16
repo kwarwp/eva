260d9797c46731b64c97ce24d4e6e74a0f3e9837
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:21:24 -0300

    Saved callie/main.py at Timestamp: 2019-03-16 14:21:24

--- callie/main.py
+++ callie/main.py
@@ -1,5 +1,5 @@
 # cibele
-from _spy.vittolino.main import Cena, Elemento,Texto
+from _spy.vitollino.main import Cena, Elemento,Texto
 linkDoGato ="https://vignette.wikia.nocookie.net/character-stats-and-profiles/images/0/09/Garfield.png/revision/latest?cb=20170701143047"
 def Historia():
 	cenaHouse = Cena (img = "https://www.simsnetwork.com/sites/simsnetwork.com/files/styles/box-downloads/public/20140218-downloads-arbuckle-03.jpg?itok=TcZfhuU-")
