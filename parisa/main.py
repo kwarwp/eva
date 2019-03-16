b11867e405fd1875a91586e44a857b156808ddec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:20:51 -0300

    Saved parisa/main.py at Timestamp: 2019-03-16 15:20:51

--- parisa/main.py
+++ parisa/main.py
@@ -8,6 +8,6 @@ def Historia():
                              style=dict(left=150, top=60, windth=60, height=200))
 	tomandJerry.entra(cenaHouse)
 	txtDoTomandJerry = Texto (cenaHouse,"Hello")
-	tomandJerry.vai=txttomandJerry.vai
+	tomandJerry.vai=txtDoTomandJerry.vai
 	cenaHouse.vai()
 Historia()
