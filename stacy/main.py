3b530828b64f76c619c3f67f6d61ca6ca1057653
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:44:25 -0300

    Created stacy/main.py at Timestamp: 2019-03-16 13:44:25

---
+++ stacy/main.py
@@ -0,0 +1,3 @@
+# Luana
+from _spy.vittolino.main import Cena, Elemento,Texto
+linkDaSereia ="https://www.google.com/search?q=fundo+do+mar&safe=strict&rlz=1C1SQJL_pt-BRBR840BR840&tbm=isch&source=lnt&tbs=ic:trans&sa=X&ved=0ahUKEwjNhMim44bhAhUiErkGHaVgC2wQpwUIIA&biw=1366&bih=657&dpr=1#imgrc=s7aRu0dOHi96bM:"
