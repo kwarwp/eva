0e3889b545e97c1cb660259c2bd235948cecae72
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:46:18 -0300

    Saved callie/main.py at Timestamp: 2019-03-16 13:46:18

--- callie/main.py
+++ callie/main.py
@@ -1,3 +1,3 @@
-# cibelo
+# cibele
 from _spy.vittolino.main import Cena, Elemento,Texto
-linkDoGato =""
+linkDoGato ="https://vignette.wikia.nocookie.net/character-stats-and-profiles/images/0/09/Garfield.png/revision/latest?cb=20170701143047"
