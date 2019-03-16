8f755fc53ad5bbdbd829a415f68ce921a1482a70
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:12:54 -0300

    Saved natalia/__log__.py at Timestamp: 2019-03-16 15:12:54

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -172,3 +172,21 @@ NameError: name 'cahorro' is not defined
     	Cahorro.vai = txtCachorro.vai
 NameError: name 'Cahorro' is not defined
 '''},
+{'date': 'Sat Mar 16 2019 12:12:55.218 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    Historia()
+  module <module> line 11
+    	cahorro.vai = txtCachorro.vai
+NameError: name 'cahorro' is not defined
+'''},
