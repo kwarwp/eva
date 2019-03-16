17295d40aa960740a79818801c14bd3e2f05d25b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:11:05 -0300

    Saved roxanne/__log__.py at Timestamp: 2019-03-16 15:11:05

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -324,3 +324,21 @@ SyntaxError: invalid syntax
     	cenaFeliz.txtfaniquita.vai
 AttributeError: 'Cena' object has no attribute 'txtfaniquita'
 '''},
+{'date': 'Sat Mar 16 2019 12:11:04.842 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    Historia()
+  module <module> line 11
+    	cena.Feliz.txtfaniquita.vai
+NameError: name 'cena' is not defined
+'''},
