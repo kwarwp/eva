63daf49268831618f315ffd80455c8b3cf6b324c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:57:43 -0300

    Saved parisa/main.py at Timestamp: 2019-03-16 13:57:42

--- parisa/main.py
+++ parisa/main.py
@@ -2,5 +2,5 @@
 from _spy.vittolino.main import Cena, Elemento, Texto
 linkDoTomandJerry ="https://static.farahexperiences.com/-/media/yasconnect/project/wbw/character/carousel/car"
 def Historia():
-
+>     cenaHouse = Cena (img ="")
 Historia()
