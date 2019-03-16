591d301795451880222177bcb5eef64c6dc3c82f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:20:07 -0300

    Saved stacy/main.py at Timestamp: 2019-03-16 14:20:07

--- stacy/main.py
+++ stacy/main.py
@@ -3,4 +3,5 @@ from _spy.vittolino.main import Cena, Elemento,Texto
 linkDaSereia "https://www.impaktovisual.com.br/2924-large_default/display-pequena-sereia.jpg"
 def Historia():
 	cenafundodomar = Cena (img = "https://img.elo7.com.br/product/original/132A83E/painel-fundo-do-mar-g-frete-gratis-festa-de-aniversario.jpg")
+	cenafundodomar.vai()
 Historia()
