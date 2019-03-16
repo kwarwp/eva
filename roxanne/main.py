66c61ce925262a960662f196d8fdd08ff8f49a32
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:49:03 -0300

    Saved roxanne/main.py at Timestamp: 2019-03-16 14:49:01

--- roxanne/main.py
+++ roxanne/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento,Texto
 linkFaniquita ="https://i.pinimg.com/originals/53/48/7d/53487d3650ef3eb22f3c8f29ad1e09a9.png"
 def Historia():
 	cenafeliz = Cena (img = "http://4.bp.blogspot.com/-Ys6mlN-qZaE/VXgmiaGmdyI/AAAAAAAAGoA/fIt-6VwUrdI/s1600/fundos-para-montagens-de-fotos-infantil%2B%25285%2529.jpg")
-	faniquita = Elemento (img =linkfaniquita,
+	faniquita = Elemento (img = linkfaniquita,
                            tit="Faniquita",
                            style=dict(left=150,top=60 width=60, height=200))
 faniquita.entra(cenafeliz)
