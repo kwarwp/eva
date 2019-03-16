f71e18b16fb1549a11d5666cc2eac50a88c7f201
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:48:59 -0300

    Saved stacy/__log__.py at Timestamp: 2019-03-16 14:48:59

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -27,3 +27,10 @@ SyntaxError: invalid syntax
                                                                                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Mar 16 2019 11:49:00.53 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  sereia = Elemento (img = linkDaSereia,
+                                                                                                                                                         ^
+SyntaxError: invalid syntax
+'''},
