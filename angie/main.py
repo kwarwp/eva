69d7bf1107616d75a42713a81a80ce54da843206
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:24:20 -0300

    Saved angie/main.py at Timestamp: 2019-03-16 14:24:20

--- angie/main.py
+++ angie/main.py
@@ -2,6 +2,6 @@
 from _spy.vitolino.main import Cena, Elemento , Texto
 linkDaTemari = "https://i.pinimg.com/originals/63/64/cf/6364cf82ddf68873bae992d2b9331128.jpg"
 def Historia(): 
-	cenaAldeiadaAreia = (img = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiI2NGP6IbhAhX9K7kGHe9LDe8QjRx6BAgBEAU&url=http%3A%2F%2Fquerodetudoumpoco.blogspot.com%2F2017%2F07%2Fum-pouco-sobre-pais-do-vento-naruto.html&psig=AOvVaw2EWw4UHt5cnL48n2wnqZ4h&ust=1552830855452569"
+	cenaAldeiadaAreia = (img = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi92_G17obhAhWYILkGHQl_AvAQjRx6BAgBEAU&url=https%3A%2F%2Faminoapps.com%2Fc%2Fnaruto-shippuden-online%2Fpage%2Fblog%2Fterceira-guerra-ninja%2Fq3Xl_37iRuBLWdb7dN5DrXekq7re6Gx1Wa&psig=AOvVaw1fQ3zN2UVVOCTb8XYKVE62&ust=1552831209833668"
 	cenaAldeiadaAreia.vai()
 Historia()
