2414b2475be344f38fd39fcbed88ba60ab36a97f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:24:29 -0300

    Saved libby/__log__.py at Timestamp: 2019-03-16 14:24:29

--- libby/__log__.py
+++ libby/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
                                                                                                                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Mar 16 2019 11:24:29.332 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  cenaPraiamoana = Cena (img = "https://img.elo7.com.br/product/original/16965E0/painel-praia-g-frete-gratis-infanitl.jpg"
+                                                                                                                                    ^
+SyntaxError: invalid syntax
+'''},
