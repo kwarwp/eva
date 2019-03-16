511acd1e419944f4e3d4aadd48aa694c216319ee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:48:20 -0300

    Saved courtney/main.py at Timestamp: 2019-03-16 13:48:20

--- courtney/main.py
+++ courtney/main.py
@@ -1,3 +1,5 @@
 # Samara
 from _spy.vittolino.main import Cena, Elemento, Texto
 linkDaMafalda ="https://i.pinimg.com/originals/18/0c/87/180c8759eec9badd395fd132859ddbc7.png"
+def Historia():
+Historia()
