5929750e23e78d66cb42a6e07522522ff9b60d9a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:23:31 -0300

    Saved stacy/__log__.py at Timestamp: 2019-03-16 14:23:30

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Mar 16 2019 11:23:31.112 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  linkDaSereia "https://www.impaktovisual.com.br/2924-large_default/display-pequena-sereia.jpg"
+                ^
+SyntaxError: invalid syntax
+'''},
