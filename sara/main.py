6188db415b6961d75f2aabb9766fd6024ea01a10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:01:07 -0300

    Saved sara/main.py at Timestamp: 2019-03-16 15:01:06

--- sara/main.py
+++ sara/main.py
@@ -4,8 +4,9 @@ linkxango = "https://i.pinimg.com/originals/a9/91/3e/a9913ee60a08b68d18d2022010a
 def Historia():
 	cenafloresta = Cena https://www.mundoecologia.com.br/wp-content/gallery/ecologia-de-plantas/Ecologia-de-Plantas-4.png= "https://www.mundoecologia.com.br/wp-content/gallery/ecologia-de-plantas/Ecologia-de-Plantas-4.png")
 	xango = Elemento(img=linkxango,
-                      tit='xango',
-                      style=edict(left=150,top =60,width=60,height=200)
+                      tit="xango",
+                      style=edict(left=150,top =60,width=60,height=200))
+	
 	cenafloresta.vai()
 Historia()
 
