40f4b6b8ff060f0de3d87fe246fa69e10aa6ac46
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:14:24 -0300

    Saved sara/__log__.py at Timestamp: 2019-03-16 15:14:24

--- sara/__log__.py
+++ sara/__log__.py
@@ -6,3 +6,10 @@
                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Mar 16 2019 12:14:23.637 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  cenafloresta = Cena https://www.mundoecologia.com.br/wp-content/gallery/ecologia-de-plantas/Ecologia-de-Plantas-4.png= "https://www.mundoecologia.com.br/wp-content/gallery/ecologia-de-plantas/Ecologia-de-Plantas-4.png")
+                       ^
+SyntaxError: invalid syntax
+'''},
