3bc0ca133fb63b7bf92b938178d2d4334c38c38f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:57:35 -0300

    Saved callie/main.py at Timestamp: 2019-03-16 13:57:34

--- callie/main.py
+++ callie/main.py
@@ -1,3 +1,6 @@
 # cibele
 from _spy.vittolino.main import Cena, Elemento,Texto
 linkDoGato ="https://vignette.wikia.nocookie.net/character-stats-and-profiles/images/0/09/Garfield.png/revision/latest?cb=20170701143047"
+def Historia():
+	cenaHouse = Cena (img = "")
+Historia()
