a2098824ba96f32227bbeb476cefc55875345573
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:45:20 -0300

    Saved stacy/main.py at Timestamp: 2019-03-16 14:45:20

--- stacy/main.py
+++ stacy/main.py
@@ -4,6 +4,7 @@ linkDaSereia ="https://www.impaktovisual.com.br/2924-large_default/display-peque
 def Historia():
 	cenafundodomar = Cena (img = "https://img.elo7.com.br/product/original/132A83E/painel-fundo-do-mar-g-frete-gratis-festa-de-aniversario.jpg")
 	sereia = Elemento (imag = linkDaSereia,
-    
+                        tit="Mar",
+                        style=dict(left=150, top=60, width=60, height=200)
 	cenafundodomar.vai()
 Historia()
