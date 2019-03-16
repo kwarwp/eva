f65a1a53990ac858b8862ef75ad20af0ede08512
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:21:03 -0300

    Saved kathryn/main.py at Timestamp: 2019-03-16 14:21:03

--- kathryn/main.py
+++ kathryn/main.py
@@ -3,4 +3,5 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 linkehuke="http://1.bp.blogspot.com/-5xVP_5A54b0/UFy5R_8BX0I/AAAAAAAAEIE/xWkWUQ4FFF8/s1600/Hulk+-+O+%C3%9Altimo+Tit%C3%A3+(1).gif"				
 def Historia():
 	cenaPly= cena (img = "http://www.pureviagem.com.br/noticia/as-10-montanhas-mais-altas-do-mundo_a9087/11#11")
+	cenaPly.vai
 Hitoria()
