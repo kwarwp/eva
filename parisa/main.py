8261cbf8b7e2f93f49ba49bf052862fa60f29a39
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:05:14 -0300

    Saved parisa/main.py at Timestamp: 2019-03-16 15:05:14

--- parisa/main.py
+++ parisa/main.py
@@ -1,8 +1,8 @@
 # sidonia
 from _spy.vitollino.main import Cena, Elemento, Texto
-linkDoTomandJerry = "https://static.farahexperiences.com/-/media/yasconnect/project/wbw/character/carousel/cartoon-junction/tom_and_jerry.ashxtom_and_jerry.ashx"
+linkDoTomandJerry = https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS76gDxCl_SENmx4-FjMGDWSi4TZzYE-__Yn1WYOzytmCGxsXnbon-junction/tom_and_jerry.ashxtom_and_jerry.ashx"
 def Historia():
-	cenaHouse = Cena (img ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1AU55ME39mqUaas9E_eu_1YquG8lOU-vNnj-nGpEza1DckseESA")
+	cenaHouse = Cena (img ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS76gDxCl_SENmx4-FjMGDWSi4TZzYE-__Yn1WYOzytmCGxsXnbU-vNnj-nGpEza1DckseESA")
 	tomandJerry = Elemento (img =linkDoTomandJerry,
                              tit="TomandJerry",
                              style=dict(left=150, top=60, windth=60, height=200))
