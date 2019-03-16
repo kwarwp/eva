193cd3522db45bcc1fb9d25dbfc202b7275c5867
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:50:57 -0300

    Saved natalia/__log__.py at Timestamp: 2019-03-16 14:50:56

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -76,3 +76,10 @@ SyntaxError: invalid syntax
                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Mar 16 2019 11:50:57.787 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  apolo = Elemento (img = LinkDoCachorro,
+                                                                      ^
+SyntaxError: invalid syntax
+'''},
