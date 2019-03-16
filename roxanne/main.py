ad1a192536eb5b0b08f2ad65db844cd9d099dcf2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:11:00 -0300

    Saved roxanne/main.py at Timestamp: 2019-03-16 15:11:00

--- roxanne/main.py
+++ roxanne/main.py
@@ -8,6 +8,6 @@ def Historia():
                            style=dict(left=70, top=60, width=100, height=60))
 	faniquita.entra(cenaFeliz)
 	txtfaniquita = Texto (cenaFeliz, "Hello")
-	cenaFeliz.txtfaniquita.vai
+	cena.Feliz.txtfaniquita.vai
 	cenaFeliz.vai()
 Historia()
