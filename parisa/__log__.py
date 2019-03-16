4af563bff3e249d597cdf4cb1d34ffa1258f3a1f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:52:51 -0300

    Saved parisa/__log__.py at Timestamp: 2019-03-16 14:52:51

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -97,3 +97,10 @@ SyntaxError: EOL while scanning string literal
                    ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Sat Mar 16 2019 11:52:50.147 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  style=dict(left=150, top=60, windth=60,height=200))
+  ^
+IndentationError: unexpected indent
+'''},
