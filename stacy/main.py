8df731f5435073195b70acdb1c68ef0a7a44ca74
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:48:55 -0300

    Saved stacy/main.py at Timestamp: 2019-03-16 14:48:54

--- stacy/main.py
+++ stacy/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento,Texto
 linkDaSereia ="https://www.impaktovisual.com.br/2924-large_default/display-pequena-sereia.jpg"
 def Historia():
 	cenafundodomar = Cena (img = "https://img.elo7.com.br/product/original/132A83E/painel-fundo-do-mar-g-frete-gratis-festa-de-aniversario.jpg")
-	sereia = Elemento (imag = linkDaSereia,
+	sereia = Elemento (img = linkDaSereia,
                         tit="Mar",
                         style=dict(left=150, top=60, width=60, height=200)
 	sereia.entra(cenadofundodomar)
