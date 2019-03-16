db5afa1a06c82e7356a4962dec7741bf58d13e51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:21:27 -0300

    Saved angie/main.py at Timestamp: 2019-03-16 14:21:27

--- angie/main.py
+++ angie/main.py
@@ -1,6 +1,7 @@
 # Maryne
-from _spy.vittolino.main import Cena, Elemento , Texto
+from _spy.vitolino.main import Cena, Elemento , Texto
 linkDaTemari ="https://i.pinimg.com/originals/63/64/cf/6364cf82ddf68873bae992d2b9331128.jpg"
 def Historia(): 
 	cenaAldeiadaAreia = (img = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiI2NGP6IbhAhX9K7kGHe9LDe8QjRx6BAgBEAU&url=http%3A%2F%2Fquerodetudoumpoco.blogspot.com%2F2017%2F07%2Fum-pouco-sobre-pais-do-vento-naruto.html&psig=AOvVaw2EWw4UHt5cnL48n2wnqZ4h&ust=1552830855452569"
+	cenaAldeiadaAreia.vai()
 Historia()
