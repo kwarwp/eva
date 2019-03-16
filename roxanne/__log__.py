ffbe1142b54f526ca20fc2513f0649aabdb37170
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:12:16 -0300

    Saved roxanne/__log__.py at Timestamp: 2019-03-16 15:12:16

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -342,3 +342,21 @@ AttributeError: 'Cena' object has no attribute 'txtfaniquita'
     	cena.Feliz.txtfaniquita.vai
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Sat Mar 16 2019 12:12:16.484 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    Historia()
+  module <module> line 11
+    	faniquita.vai = txtFaniquita.vai
+NameError: name 'txtFaniquita' is not defined
+'''},
