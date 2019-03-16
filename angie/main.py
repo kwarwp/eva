ee84b67e661ff4e59536fc48a58b93d5bd9442b9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:42:54 -0300

    Saved angie/main.py at Timestamp: 2019-03-16 14:42:54

--- angie/main.py
+++ angie/main.py
@@ -5,6 +5,6 @@ def Historia():
 	cenaAldeiadaAreia = Cena (img = "https://pm1.narvii.com/6398/faaf9efd310bba5b5fc7ee68cb18ca138d43e30b_hq.jpg")
 	Temari = Elemento (img =  linkDaTemari,
                         tit="Temari",
-                        style
+                        style=dict(left=150,)
     cenaAldeiadaAreia.vai()
 Historia()
