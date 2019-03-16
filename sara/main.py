7db17401c8dc4582b859914d9e916c2f963dbeb0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:46:13 -0300

    Saved sara/main.py at Timestamp: 2019-03-16 14:46:13

--- sara/main.py
+++ sara/main.py
@@ -2,6 +2,9 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 linkxango = "https://i.pinimg.com/originals/a9/91/3e/a9913ee60a08b68d18d2022010acf7fb.png"
 def Historia():
-	cenafloresta = Cena (img = "https://www.mundoecologia.com.br/wp-content/gallery/ecologia-de-plantas/Ecologia-de-Plantas-4.png")
+	cenafloresta = Cena https://www.mundoecologia.com.br/wp-content/gallery/ecologia-de-plantas/Ecologia-de-Plantas-4.png= "https://www.mundoecologia.com.br/wp-content/gallery/ecologia-de-plantas/Ecologia-de-Plantas-4.png")
+	xango = element(img-=link xango
 	cenafloresta.vai()
 Historia()
+tit='xango',
+style=edict
