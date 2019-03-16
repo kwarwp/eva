2d9f1f222d942312c94ef450f5d478372e31c15f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:49:58 -0300

    Saved kathryn/main.py at Timestamp: 2019-03-16 13:49:58

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,1 +1,6 @@
 # alex
+from _spy.vittolino.main import Cena, Elemento, Texto
+linkehuke="http://1.bp.blogspot.com/-5xVP_5A54b0/UFy5R_8BX0I/AAAAAAAAEIE/xWkWUQ4FFF8/s1600/Hulk+-+O+%C3%9Altimo+Tit%C3%A3+(1).gif"				
+def Historia():
+
+Hitoria()
