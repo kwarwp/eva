0ffcb6ad650839d1e4a42fd56f2d23fed395d002
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:50:48 -0300

    Saved libby/main.py at Timestamp: 2019-03-16 13:50:47

--- libby/main.py
+++ libby/main.py
@@ -1,3 +1,6 @@
 # Lucimara
 from _spy.vittolino.main import: Cena, Elemento, Texto
 linkdamoana ="https://vignette.wikia.nocookie.net/characterprofile/images/6/64/Hertmoana.png/revision/latest?cb=20180112235349"
+def Historia():
+
+Historia()
