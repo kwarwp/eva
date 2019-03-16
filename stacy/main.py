8b60becc2d6b90916b7941b74772b554594fbe14
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:01:23 -0300

    Saved stacy/main.py at Timestamp: 2019-03-16 14:01:23

--- stacy/main.py
+++ stacy/main.py
@@ -1,6 +1,6 @@
 # Luana
 from _spy.vittolino.main import Cena, Elemento,Texto
-linkDaSereia ="https://www.google.com/search?q=fundo+do+mar&safe=strict&rlz=1C1SQJL_pt-BRBR840BR840&tbm=isch&source=lnt&tbs=ic:trans&sa=X&ved=0ahUKEwjNhMim44bhAhUiErkGHaVgC2wQpwUIIA&biw=1366&bih=657&dpr=1#imgrc=s7aRu0dOHi96bM:"
+linkDaSereia "https://www.impaktovisual.com.br/2924-large_default/display-pequena-sereia.jpg"
 def Historia():
-
+	cenafundodomar = Cena (img = "https://img.elo7.com.br/product/original/132A83E/painel-fundo-do-mar-g-frete-gratis-festa-de-aniversario.jpg")
 Historia()
