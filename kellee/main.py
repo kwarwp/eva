9ad632f2e224bec7405d4fcbeb85f34f7d30ab45
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:20:05 -0300

    Saved kellee/main.py at Timestamp: 2019-03-16 14:20:05

--- kellee/main.py
+++ kellee/main.py
@@ -3,4 +3,5 @@ from _spy.vittolino.main import Cena, Elemento, Texto
 linkDoMickey ="https://i.pinimg.com/originals/57/dc/a9/57dca93157d27dd05ef7657c4c0ba07a.png"
 def Historia():
 	cenaParque = Cena (img = "http://starkovtattoo.spb.ru/images/400/DSC100466834.jpg")
+	cenaParque.vai()
 Historia()
