51dd576951eadc8f81845f9b5a63c29d61d23ba6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:24:33 -0300

    Saved parisa/__log__.py at Timestamp: 2019-03-16 14:24:33

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -41,3 +41,10 @@ SyntaxError: invalid syntax
                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Mar 16 2019 11:24:32.134 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  linkDoTomandJerry=https://static.farahexperiences.com/-/media/yasconnect/project/wbw/character/carousel/cartoon-junction/tom_and_jerry.ashx
+                          ^
+SyntaxError: invalid syntax
+'''},
