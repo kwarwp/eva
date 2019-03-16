ce8478957063d91676be804a0e62b0454f9b7720
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:22:13 -0300

    Saved angie/__log__.py at Timestamp: 2019-03-16 14:22:12

--- angie/__log__.py
+++ angie/__log__.py
@@ -6,3 +6,10 @@
                                                                                                                                                                                                                                                                                                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Mar 16 2019 11:22:12.740 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  cenaAldeiadaAreia = (img = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiI2NGP6IbhAhX9K7kGHe9LDe8QjRx6BAgBEAU&url=http%3A%2F%2Fquerodetudoumpoco.blogspot.com%2F2017%2F07%2Fum-pouco-sobre-pais-do-vento-naruto.html&psig=AOvVaw2EWw4UHt5cnL48n2wnqZ4h&ust=1552830855452569"
+                                                                                                                                                                                                                                                                                                                 ^
+SyntaxError: invalid syntax
+'''},
