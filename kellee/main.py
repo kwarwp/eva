d1f7f74da6ca2c0e7326ed901ada6f99ad775303
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:07:21 -0300

    Saved kellee/main.py at Timestamp: 2019-03-16 15:07:17

--- kellee/main.py
+++ kellee/main.py
@@ -3,10 +3,10 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 linkDoMickey ="https://vignette.wikia.nocookie.net/dublagempedia/images/6/6d/Mickey-mouse27.png/revision/latest?cb=20180110221858&path-prefix=pt-br"
 def Historia():
 	cenaParque = Cena (img = "http://starkovtattoo.spb.ru/images/400/DSC100466834.jpg")
-    
 	mickey = Elemento (img = linkDoMickey,
                         tit="Mickey",
                         style=dict(left=20, top=60, width=150, height=500))
 	mickey.entra(cenaParque)
+	txtMickey = Texto
 	cenaParque.vai()
 Historia()
