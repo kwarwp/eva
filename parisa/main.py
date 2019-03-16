d657f3b74610314d9de7b1c5daca8884940c44a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:50:49 -0300

    Saved parisa/main.py at Timestamp: 2019-03-16 13:50:48

--- parisa/main.py
+++ parisa/main.py
@@ -1,5 +1,6 @@
 # sidonia
 from _spy.vittolino.main import Cena, Elemento, Texto
-linkDoTomandJerry ="Tom and Jerry"
+linkDoTomandJerry ="https://static.farahexperiences.com/-/media/yasconnect/project/wbw/character/carousel/car"
 def Historia():
+
 Historia()
