a7f699999e14ec12d701ad0892da99a0fb04c316
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 10:50:03 -0300

    Saved natalia/main.py at Timestamp: 2019-03-16 13:50:03

--- natalia/main.py
+++ natalia/main.py
@@ -1,3 +1,6 @@
 # carlos paulo 
 from _spy.vittolino.main import Cena,Elemento,Texto
  LinkDoCachorro ="https://cdn.shopify.com/s/files/1/0934/6862/products/24223779_l_clipped_rev_1_480x480.png?v=1491220569"
+def historia():
+
+historia()
