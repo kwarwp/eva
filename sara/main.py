c5e89362154b9f1d96fd8e407cdad1ef9f605064
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:08:44 -0300

    Saved sara/main.py at Timestamp: 2019-03-16 14:08:44

--- sara/main.py
+++ sara/main.py
@@ -1,8 +1,11 @@
 # daiane
 from _spy.vittolino.main import Cena,Elemento,Texto
 linkxango="https://i.pinimg.com/originals/a9/91/3e/a9913ee60a08b68d18d2022010acf7fb.png"
-def Historia
-https://www.google.com/imgres?imgurl=https%3A%2F%2Fpaijoaodeangola.com%2Fwp-content%2Fuploads%2F2019%2F01%2Fumbanda_filosofia.png.opt228x265o00s228x265.png&imgrefurl=https%3A%2F%2Fpaijoaodeangola.com%2Fo-compromisso-assumido%2F&docid=yWFNRlqz5SE6tM&tbnid=NTeQkiW3XTjcKM%3A&vet=12ahUKEwjGku7L6IbhAhWwIbkGHdE2Cfk4yAEQMygCMAJ6BAgBEAM..i&w=228&h=265&safe=strict&bih=662&biw=1366&q=casa%20do%20xango&ved=2ahUKEwjGku7L6IbhAhWwIbkGHdE2Cfk4yAEQMygCMAJ6BAgBEAM&iact=mrc&uact=8
+def Historia():
+	cenafloresta=cena(img="")
+Historia():
+
+
 
 
 
