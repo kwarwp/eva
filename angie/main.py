b3fa71b040a78fcea3fb2770d533fa50a5ad9982
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:31:17 -0300

    Saved angie/main.py at Timestamp: 2019-03-16 14:31:17

--- angie/main.py
+++ angie/main.py
@@ -2,6 +2,6 @@
 from _spy.vitollino.main import Cena, Elemento , Texto
 linkDaTemari = "https://i.pinimg.com/originals/63/64/cf/6364cf82ddf68873bae992d2b9331128.jpg"
 def Historia(): 
-	cenaAldeiadaAreia = Cena (img = "https://pm1.narvii.com/6398/faaf9efd310bba5b5fc7ee68cb18ca138d43e30b_hq.jpg"
+	cenaAldeiadaAreia = Cena (img = "https://pm1.narvii.com/6398/faaf9efd310bba5b5fc7ee68cb18ca138d43e30b_hq.jpg")
 	cenaAldeiadaAreia.vai()
 Historia()
