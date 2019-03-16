954691cab980030c762b8ecded5782ad130ce7f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:56:36 -0300

    Saved parisa/main.py at Timestamp: 2019-03-16 14:56:36

--- parisa/main.py
+++ parisa/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 linkDoTomandJerry = "https://static.farahexperiences.com/-/media/yasconnect/project/wbw/character/carousel/cartoon-junction/tom_and_jerry.ashxtom_and_jerry.ashx"
 def Historia():
 	cenaHouse = Cena (img ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1AU55ME39mqUaas9E_eu_1YquG8lOU-vNnj-nGpEza1DckseESA")
-	TomandJerry= Elemento (img = linkdooTmandJerry,
+	TomandJerry= Elemento (img = linkdooTmandJerry
                             tit="TomandJerry"
                             style=dict(left=150, top=60, windth=60,height=200))
 	TomandJerry.entra(cenaHouse)
