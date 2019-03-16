10db37df253f475d885fc95a7523d65d3fd76ad4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:38:51 -0300

    Created sara/__log__.py at Timestamp: 2019-03-16 14:38:51

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Mar 16 2019 11:38:49.455 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  def Historia()
+                 ^
+SyntaxError: invalid syntax
+'''},
