ba75da88c5ff259aa351a97782b3f805ec68a285
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:21:39 -0300

    Saved libby/main.py at Timestamp: 2019-03-16 14:21:39

--- libby/main.py
+++ libby/main.py
@@ -1,5 +1,5 @@
 # Lucimara
-from _spy.vittolino.main import: Cena, Elemento, Texto
+from _spy.vitollino.main import: Cena, Elemento, Texto
 linkdamoana ="https://vignette.wikia.nocookie.net/characterprofile/images/6/64/Hertmoana.png/revision/latest?cb=20180112235349"
 def Historia():
 	cenaPraiamoana = Cena (img + "https://img.elo7.com.br/product/original/16965E0/painel-praia-g-frete-gratis-infanitl.jpg"
