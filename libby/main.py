b714650d33527c1919c9a490fd1a42dbe2483081
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:20:06 -0300

    Saved libby/main.py at Timestamp: 2019-03-16 14:20:06

--- libby/main.py
+++ libby/main.py
@@ -3,5 +3,6 @@ from _spy.vittolino.main import: Cena, Elemento, Texto
 linkdamoana ="https://vignette.wikia.nocookie.net/characterprofile/images/6/64/Hertmoana.png/revision/latest?cb=20180112235349"
 def Historia():
 	cenaPraiamoana = Cena (img + "https://img.elo7.com.br/product/original/16965E0/painel-praia-g-frete-gratis-infanitl.jpg"
+       	cenaPraiamoana.vai ()
         Historia()
         
