3973e448295674356fd1612cb771912ed4fe90dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:38:16 -0300

    Saved sara/main.py at Timestamp: 2019-03-16 14:38:15

--- sara/main.py
+++ sara/main.py
@@ -2,6 +2,6 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 linkxango = "https://i.pinimg.com/originals/a9/91/3e/a9913ee60a08b68d18d2022010acf7fb.png"
 def Historia()
-	 cenafloresta=cena
+	cenafloresta = Cena (img = "https://www.mundoecologia.com.br/wp-content/gallery/ecologia-de-plantas/Ecologia-de-Plantas-4.png")
 	cena=cenafloresta
 Historia()
