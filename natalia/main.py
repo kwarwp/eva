ac77153ba1974e4c4fb44a44cfb76d7d4975d126
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 12:24:22 -0300

    Saved natalia/main.py at Timestamp: 2019-03-16 15:24:21

--- natalia/main.py
+++ natalia/main.py
@@ -2,7 +2,7 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 LinkDoCachorro = "https://cdn.shopify.com/s/files/1/0934/6862/products/10092191_l_clipped_rev_1_480x480.png?v=1491220521"
 def Historia():
-	cenaCout = Cena (img = "https://q-ec.bstatic.com/images/hotel/max1024x768/837/83757797.jpg")
+	cenaCout = Cena (img = "https://http2.mlstatic.com/sementes-da-melhor-grama-p-jardim-folha-fina-ideal-60-m-D_NQ_NP_524601-MLB20356040711_072015-F.jpg")
 	apolo = Elemento (img = LinkDoCachorro,
                         tit="apolo",
                         style=dict(left=60, top=60, widt=60, height=200))
