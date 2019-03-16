0e17aa63d116463737a1f2a22b3d9f24974d0573
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:30:20 -0300

    Saved sara/main.py at Timestamp: 2019-03-16 14:30:19

--- sara/main.py
+++ sara/main.py
@@ -1,5 +1,6 @@
 # daiane
 from _spy.vittolino.main import Cena,Elemento,Texto
 linkxango = "https://i.pinimg.com/originals/a9/91/3e/a9913ee60a08b68d18d2022010acf7fb.png"
-def Historia():
-
+def Historia()
+	 cenafloresta=cena
+	cena=cenafloresta
