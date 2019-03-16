3148ab8618d3aa2f55cd6b0e9e68c3b2ac63c7d5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-03-16 11:41:56 -0300

    Saved libby/main.py at Timestamp: 2019-03-16 14:41:55

--- libby/main.py
+++ libby/main.py
@@ -4,6 +4,7 @@ linkdamoana ="https://vignette.wikia.nocookie.net/characterprofile/images/6/64/H
 def Historia():
 	cenaPraiamoana = Cena (img = "https://img.elo7.com.br/product/original/16965E0/painel-praia-g-frete-gratis-infanitl.jpg")
 	moana = Elemento (img = linkdamoana, 
-    cenaPraiamoana.vai()
+                       tit="moana",
+	cenaPraiamoana.vai()
 Historia()
         
