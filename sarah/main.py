28c3254839a08628ef1c1d998012a1a69cca62de
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:30:25 -0300

    Saved sarah/main.py at Timestamp: 2019-05-04 15:30:24

--- sarah/main.py
+++ sarah/main.py
@@ -7,7 +7,7 @@ def Historia():
                        tit="Robinson Crusoe",
                        style=dict(left=10, top=60,  Width=60, height=50))
 	moana.entra(cena3)                       
-	txtmoana = Texto (cena3, "Hello")
+	txtmoana = Texto (cena3, "For two years I lived a slave life, until I managed to escape using a fishing boat. But I was still in Africa, afraid of animals and the dangers that could be there. He was drifting for a while until being rescued by a Portuguese ship that took him to Brazil.")
 	moana.vai = txtmoana.vai
 	cena3.vai ()
 Historia()# eva.sarah.main.py
