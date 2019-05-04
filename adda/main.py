d58e0869c7a884e3ac0f975fbf5e216a19056e02
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:27:25 -0300

    Saved adda/main.py at Timestamp: 2019-05-04 15:27:25

--- adda/main.py
+++ adda/main.py
@@ -7,7 +7,7 @@ def Historia():
                                tit="Robinson Crusoe",
                                style=dict(left=30, top=60, width=180, height=100))
 	pequenasereia.entra(cena4)
-	txtpequenasereia = Texto (cena4, "Hello")
+	txtpequenasereia = Texto (cena4, "By morning the sea was calmer, and to my surprise the ship was there, so I swam to it and managed to get through a hole. A part of the ship was out of the water just where the food was and I took some food to the beach, along with the food I also took knives and other tools that I found on the ship. I decided to build a small house so I used the woods and sails of the ship, I went back a few times to get some things but after a few days there was a big storm and the ship disappeared into the sea. I was happy because despite being alone on the island I was alive and with tools and food.")
 	pequenasereia.vai = txtpequenasereia.vai()
 	cena4.vai()
 Historia()
