9d32c4a7c9e5a3696b90b636b35182dc3b71c913
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:25:23 -0300

    Saved amanda/main.py at Timestamp: 2019-05-04 15:25:23

--- amanda/main.py
+++ amanda/main.py
@@ -4,7 +4,7 @@ transparente ="https://www.transparenttextures.com/patterns/debut-light.png"
 def Historia():
 	cena1 = Cena (img = "https://i.imgur.com/0mwf17P.png")
 	gato = Elemento (img = transparente,
-                      tit="Garfield",
+                      tit="Robinson Crusoe",
                       style=dict(left=150, top=60, width=60, height=200))
 	gato.entra(cena1)
 	txtGato = Texto (cena1, "Hello")
