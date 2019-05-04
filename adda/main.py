b5ec824d4425787ac1eebf12e6bbdf365008f091
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:14:52 -0300

    Saved adda/main.py at Timestamp: 2019-05-04 15:14:51

--- adda/main.py
+++ adda/main.py
@@ -1,9 +1,9 @@
-# Luana
+# Luana RC
 from _spy.vitollino.main import Cena, Elemento,Texto
-linkDaSereia ="https://www.impaktovisual.com.br/2924-large_default/display-pequena-sereia.jpg"
+transparente ="https://www.transparenttextures.com/patterns/debut-light.png"
 def Historia():
 	cenaFundodomar = Cena (img = "https://img.elo7.com.br/product/original/132A83E/painel-fundo-do-mar-g-frete-gratis-festa-de-aniversario.jpg")
-	pequenasereia = Elemento (img = linkDaSereia,
+	pequenasereia = Elemento (img = transparente,
                                tit="Mar",
                                style=dict(left=30, top=60, width=180, height=100))
 	pequenasereia.entra(cenaFundodomar)
