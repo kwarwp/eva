d8fd9b656402d5c76f292d9d074bce2a7b94ad1c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:22:38 -0300

    Created sarah/main.py at Timestamp: 2019-05-04 15:22:38

---
+++ sarah/main.py
@@ -0,0 +1,15 @@
+# Lucimara
+from _spy.vitollino.main import Cena, Elemento, Texto
+transparente ="https://www.transparenttextures.com/patterns/debut-light.png"
+def Historia():
+	cenaPraiamoana = Cena 3(img = "https://i.imgur.com/I2u7XCz.png 
+
+")
+	moana = Elemento (img = linkdamoana, 
+                       tit="moana",
+                       style=dict(left=10, top=60,  Width=60, height=50))
+	moana.entra(cenaPraiamoana)                       
+	txtmoana = Texto (cenaPraiamoana, "Hello")
+	moana.vai = txtmoana.vai
+	cenaPraiamoana.vai ()
+Historia()# eva.sarah.main.py
