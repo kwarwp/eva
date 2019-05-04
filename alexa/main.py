0fe23fbab34dd13b51912d5235eccf04ad132886
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:26:57 -0300

    Saved alexa/main.py at Timestamp: 2019-05-04 15:26:57

--- alexa/main.py
+++ alexa/main.py
@@ -4,10 +4,10 @@ transparente ="https://www.transparenttextures.com/patterns/debut-light.png"
 def Historia():
 	cena2 = Cena (img = "https://i.imgur.com/wQmMx4O.png")
 	mickey = Elemento (img = transparente,
-                        tit="Mickey",
+                        tit="Robinson Crusoe",
                         style=dict(left=20, top=60, width=150, height=500))
 	mickey.entra(cena2)
-	txtMickey = Texto (cena2, "Hello")
+	txtMickey = Texto (cena2, "For two years I lived a slave life, until I managed to escape using a fishing boat. But I was still in Africa, afraid of animals and animals that exist there. He was adrift for a time until he was rescued by a Portuguese ship that was taking him to Brazil.")
 	mickey.vai = txtMickey.vai
 	cena2.vai()
 Historia()
