c2e018b867886e9da7ad7f5fa70797be9d77cc2a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:10:18 -0300

    Saved amanda/main.py at Timestamp: 2019-05-04 15:10:17

--- amanda/main.py
+++ amanda/main.py
@@ -1,6 +1,6 @@
 # cibele RC
 from _spy.vitollino.main import Cena, Elemento,Texto
-linkDoGato ="https://vignette.wikia.nocookie.net/character-stats-and-profiles/images/0/09/Garfield.png/revision/latest?cb=20170701143047"
+trasparente ="https://www.transparenttextures.com/patterns/debut-light.png"
 def Historia():
 	cenaHouse = Cena (img = "https://www.simsnetwork.com/sites/simsnetwork.com/files/styles/box-downloads/public/20140218-downloads-arbuckle-03.jpg?itok=TcZfhuU-")
 	gato = Elemento (img = linkDoGato,
