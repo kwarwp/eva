bdf7a61298d1d8fe5d22a6ad6ea4e6e073e16d45
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:28:16 -0300

    Saved julia/main.py at Timestamp: 2019-05-04 15:28:15

--- julia/main.py
+++ julia/main.py
@@ -7,7 +7,8 @@ def Historia():
                            tit="Robinson Crusoe",
                            style=dict(left=70, top=60, width=100, height=60))
 	faniquita.entra(cena2)
-	txtfaniquita = Texto (cena2, "Hello")
+	txtfaniquita = Texto (cena2, "
+For two years I lived a slave life, until I managed to escape using a fishing boat. But I was still in Africa, afraid of animals and the dangers that could be there. He was drifting for a while until being rescued by a Portuguese ship that took him to Brazil")
 	faniquita.vai = txtfaniquita.vai
 	cena2.vai()
 Historia()
