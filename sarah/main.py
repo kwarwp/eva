b24189864f0efdec93a932464fee5ace50a11333
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:23:20 -0300

    Saved sarah/main.py at Timestamp: 2019-05-04 15:23:20

--- sarah/main.py
+++ sarah/main.py
@@ -2,14 +2,12 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 transparente ="https://www.transparenttextures.com/patterns/debut-light.png"
 def Historia():
-	cenaPraiamoana = Cena 3(img = "https://i.imgur.com/I2u7XCz.png 
-
-")
+	cena3 = Cena (img = "https://i.imgur.com/I2u7XCz.png")
 	moana = Elemento (img = linkdamoana, 
                        tit="moana",
                        style=dict(left=10, top=60,  Width=60, height=50))
-	moana.entra(cenaPraiamoana)                       
-	txtmoana = Texto (cenaPraiamoana, "Hello")
+	moana.entra(cena3)                       
+	txtmoana = Texto (cena3, "Hello")
 	moana.vai = txtmoana.vai
-	cenaPraiamoana.vai ()
+	cena3.vai ()
 Historia()# eva.sarah.main.py
