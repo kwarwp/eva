520d51dac1c5d1e5002df2f8032548112b616d83
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:19:55 -0300

    Saved adda/main.py at Timestamp: 2019-05-04 15:19:55

--- adda/main.py
+++ adda/main.py
@@ -6,8 +6,8 @@ def Historia():
 	pequenasereia = Elemento (img = transparente,
                                tit="Mar",
                                style=dict(left=30, top=60, width=180, height=100))
-	pequenasereia.entra(cenaFundodomar)
-	txtpequenasereia = Texto (cenaFundodomar, "Hello")
+	pequenasereia.entra(cena1)
+	txtpequenasereia = Texto (cena1, "Hello")
 	pequenasereia.vai = txtpequenasereia.vai()
-	cenaFundodomar.vai()
+	cena1.vai()
 Historia()
