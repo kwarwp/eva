8602cd1ecdff864a051162e295c171296232c06d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:18:01 -0300

    Saved adda/main.py at Timestamp: 2019-05-04 15:18:00

--- adda/main.py
+++ adda/main.py
@@ -2,7 +2,7 @@
 from _spy.vitollino.main import Cena, Elemento,Texto
 transparente ="https://www.transparenttextures.com/patterns/debut-light.png"
 def Historia():
-	cenaFundodomar = Cena (img = "https://img.elo7.com.br/product/original/132A83E/painel-fundo-do-mar-g-frete-gratis-festa-de-aniversario.jpg")
+	cena4 = Cena (img = "https://i.imgur.com/K2S15Bz.png")
 	pequenasereia = Elemento (img = transparente,
                                tit="Mar",
                                style=dict(left=30, top=60, width=180, height=100))
