3ae72131c9b2664807ee62e943d9adb2b48bfe94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:19:27 -0300

    Saved julia/main.py at Timestamp: 2019-05-04 15:19:26

--- julia/main.py
+++ julia/main.py
@@ -2,7 +2,7 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 transparente ="https://www.transparenttextures.com/patterns/debut-light.png "
 def Historia():
-	cenaFeliz = Cena (img = "http://4.bp.blogspot.com/-Ys6mlN-qZaE/VXgmiaGmdyI/AAAAAAAAGoA/fIt-6VwUrdI/s1600/fundos-para-montagens-de-fotos-infantil%2B%25285%2529.jpg")
+	cenaFeliz = Cena (img = "https://i.imgur.com/wQmMx4O.png")
 	faniquita = Elemento (img = transparente,
                            tit="Faniquita",
                            style=dict(left=70, top=60, width=100, height=60))
