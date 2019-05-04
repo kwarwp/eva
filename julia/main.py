1354fea312fd62082ca0a46afb8e9e584ff394e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:21:51 -0300

    Saved julia/main.py at Timestamp: 2019-05-04 15:21:51

--- julia/main.py
+++ julia/main.py
@@ -2,12 +2,12 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 transparente ="https://www.transparenttextures.com/patterns/debut-light.png "
 def Historia():
-	cenaFeliz = Cena (img = "https://i.imgur.com/wQmMx4O.png")
+	cena2 = Cena (img = "https://i.imgur.com/wQmMx4O.png")
 	faniquita = Elemento (img = transparente,
                            tit="Faniquita",
                            style=dict(left=70, top=60, width=100, height=60))
-	faniquita.entra(cena1)
-	txtfaniquita = Texto (cena1, "Hello")
+	faniquita.entra(cena2)
+	txtfaniquita = Texto (cena2, "Hello")
 	faniquita.vai = txtfaniquita.vai
-	cena1.vai()
+	cena2.vai()
 Historia()
