2590eeeebcb02ff8e9bbe2a1ff7014ee3af26677
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-04 12:14:48 -0300

    Saved amanda/main.py at Timestamp: 2019-05-04 15:14:48

--- amanda/main.py
+++ amanda/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento,Texto
 trasparente ="https://www.transparenttextures.com/patterns/debut-light.png"
 def Historia():
 	cenaHouse = Cena (img = "https://www.simsnetwork.com/sites/simsnetwork.com/files/styles/box-downloads/public/20140218-downloads-arbuckle-03.jpg?itok=TcZfhuU-")
-	gato = Elemento (img = linkDoGato,
+	gato = Elemento (img = transparente,
                       tit="Garfield",
                       style=dict(left=150, top=60, width=60, height=200))
 	gato.entra(cenaHouse)
